f42072509f57a76da47ec57d1e888858929edafe
ifeanyijasper <ifeanyijasper11@gmail.com>
2024-04-29 11:42:01 +0100

    instance api response and model serializer

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,11 +1,15 @@
 import requests
 
-endpoint = "https://httpbin.org/status/200/"
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/status/200/"
+# endpoint = "https://httpbin.org/anything"
 
-get_response = requests.get(endpoint)
+#Django Server 
+endpoint = "http://localhost:8000/api/"
+
+get_response = requests.get(endpoint, params={"abc":123}, json={"query":"Hello World"})
+# print(get_response.text)
+# print(get_response.status_code)
 
 #JSON
 
 print(get_response.json())
-print(get_response.status_code)
